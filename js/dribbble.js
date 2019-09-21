1d3a9597eb24349a5bfead452ca15c67d30bf08c
JKoenen99 <jap.koenen@hotmail.nl>
2019-09-21 13:46:37 +0200

    trying to integrate dribbble into the portfolio and combining with OWL slider

--- js/dribbble.js
+++ js/dribbble.js
@@ -1,3 +1,42 @@
+// // Set the Access Token
+// var accessToken = 'aaaf79c846818aed133882b98352efaa9321da52d7cb8507bcb04de9bddba006';
+
+// // Call Dribble v2 API
+// $.ajax({
+//     url: 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken,
+//     dataType: 'json',
+//     type: 'GET',
+//     success: function(data) {  
+//       if (data.length > 0) { 
+//         $.each(data.reverse(), function(i, val) {                
+//           $('#shots').prepend(
+//             '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
+//             )
+//         })
+//       }
+//       else {
+//         $('#shots').append('<p>No shots yet!</p>');
+//       }
+//     }
+// });
+
+
+// // API Variables
+
+// //Image width 800x600 (Animated)
+//   // val.images.hidpi 
+// //Image width 400x300
+//   // val.images.normal
+// //Image width 200x150
+//   // val.images.teaser
+// //Title
+//   // val.title
+// //Description
+//   // val.description
+// //URL
+//   // val.html_url
+
+
 // Set the Access Token
 var accessToken = 'aaaf79c846818aed133882b98352efaa9321da52d7cb8507bcb04de9bddba006';
 
@@ -10,7 +49,7 @@ $.ajax({
       if (data.length > 0) { 
         $.each(data.reverse(), function(i, val) {                
           $('#shots').prepend(
-            '<a class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></a>'
+            '<a class="work_thumb shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="work-text">' + '<h3>' + val.title + '</h3>' + '</div><img class="img-fluid" src="'+ val.images.hidpi +'"/></a>'
             )
         })
       }
